f26babcdb4a79a75c046e9f9e396ffd443dc87f1
I407846 <o.aboudehn@student.fontys.nl>
2022-04-25 00:29:29 +0200

    database connectivity

--- safar/src/main/java/nl/springboot/safar/models/User.java
+++ safar/src/main/java/nl/springboot/safar/models/User.java
@@ -1,5 +1,13 @@
 package nl.springboot.safar.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+
 public class User {
 
     private int id;
--- safar/src/test/java/repositories/FakeUserDataTest.java
+++ safar/src/test/java/repositories/FakeUserDataTest.java
@@ -1,13 +1,23 @@
 package repositories;
 
+import nl.springboot.safar.models.User;
+import nl.springboot.safar.repositories.FakeUserData;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@SpringBootTest(classes = FakeUserData.class)
 public class FakeUserDataTest {
+
+    @Autowired
+    private FakeUserData fakeUserData;
 
     @Test
     public void getUserById(){
-        assertEquals(1,1);
+        User actualUser = fakeUserData.getUserBy(1);
+        User expectedUser = new User(1, "Kevin", "Breda", "Kevin@live.com");
+        assertEquals(expectedUser.getId(),actualUser.getId());
     }
 }
